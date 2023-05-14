639af0c8bc4714831ee1e4b66b8727a9e18076d8
kraisorn <22193826+tOxicV4p0r@users.noreply.github.com>
2023-05-14 10:19:06 +0700

    update

---
+++ src/components/HomeScreen/HomeScreen.js
@@ -0,0 +1,69 @@
+import React from "react";
+import Nav from "../Nav/Nav"
+import "./homescreen.css"
+
+function HomeScreen() {
+    return (
+        <div className="homescreen">
+            <Nav />
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+        </div>
+    )
+};
+
+export default HomeScreen;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import "./nav.css";
+
+function Nav() {
+    const [show, setShow] = useState(false);
+
+    const transitionNavBar = () => {
+        if (window.scrollY > 100) {
+            setShow(true);
+        } else {
+            setShow(false);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", transitionNavBar);
+        return () => window.removeEventListener("scroll", transitionNavBar);
+    }, []);
+
+    return (
+        <div className="nav nav__black">
+            <div className="nav__contents">
+                <img
+                    className="nav__logo"
+                    src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
+                    alt=""
+                />
+                <img
+                    className="nav__avatar"
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmX1IYrleu5pZkTWvD6cBrp4E0knysir8f-A&usqp=CAU"
+                    alt=""
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Nav;
